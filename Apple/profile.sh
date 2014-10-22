432fc3a16e23f31e7054d4480dfb4180962ca3c2
Ben Hamner <ben@benhamner.com>
2014-10-22 02:10:48 -0700

    updated profile

--- Apple/profile.sh
+++ Apple/profile.sh
@@ -1,3 +1,19 @@
 
-alias edit="open -a Sublime\ Text\ 2"
+bind '"\e[A"':history-search-backward
+bind '"\e[B"':history-search-forward
+bind "set completion-ignore-case on"
+bind "set show-all-if-ambiguous on"
 
+alias edit='subl'
+alias f='open -a Finder ./'
+alias julia3='/Applications/Julia-0.3.1.app/Contents/Resources/julia/bin/julia'
+alias julia='/Applications/Julia-0.4.0-dev-e22f90edc8.app/Contents/Resources/julia/bin/julia'
+
+cd() { builtin cd "$@"; ls; }               
+alias ..='cd ../'
+alias ...='cd ../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+alias .6='cd ../../../../../../'
+alias ~="cd ~"
